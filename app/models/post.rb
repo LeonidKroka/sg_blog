bed753c142de0af1598f629cf84203f5ac1171d1
Leonid Kroka <kroka_l@ukr.net>
2016-10-08 14:21:31 +0300

    Add img to post:gem carrierwave. View image in post. Fix footer's bag.
    Add img to post:gem carrierwave. View image in post. Fix footer's bag.
    
    Next footer's fix: move footer to basement of any page

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
   validates :title, format: { with: /[\s]{0,1}[a-zA-Z0-9]+/ }
   validates :body, length: { minimum: 200 }
 
+  mount_uploader :image, ImageUploader
+
   def self.latest_five
     order(created_at: :desc).limit(5)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006152340) do
+ActiveRecord::Schema.define(version: 20161007191600) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
