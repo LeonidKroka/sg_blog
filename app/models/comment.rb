8798c338fbbcd63188a0957b4144896c5bc33090
Leonid Kroka <kroka_l@ukr.net>
2016-10-11 17:48:49 +0300

    Add comments to post without edit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   validates_presence_of :body
-  validates :body, length: { maxsimum: 200}
+  validates :body, length: { maximum: 200}
 
   def self.latest_ten
     order(created_at: :desc).limit(10)
