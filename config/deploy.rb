93324397237f42ff1228c697071f36f24b4aca49
kroka_l <sg.leonidK@gmail.com>
2017-03-16 17:38:20 +0200

    Try

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,21 @@ namespace :puma do
 end
 
 namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      begin
+        from = source.next_revision(current_revision) # <-- Fail here at first-time deploy
+      rescue
+        err_no = true
+      end
+      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+  
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
