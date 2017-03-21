3a025b757cd446db85480fb1e2ea1eea22f5c4c7
kroka_l <sg.leonidK@gmail.com>
2017-03-21 15:44:34 +0200

    Change deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,7 +68,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      #invoke 'puma:restart'
       Rake::Task['puma:restart'].reenable
     end
   end
