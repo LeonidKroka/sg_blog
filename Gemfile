1e89c1a35f7afe5f45fb5d1dd191938dafc293ab
kroka_l <sg.leonidK@gmail.com>
2017-03-21 15:14:15 +0200

    Change db.

--- Gemfile
+++ Gemfile
@@ -28,7 +28,6 @@ group :test do
 end
 
 group :development do
-  gem 'sqlite3'
   gem 'web-console', '~> 2.0'
   gem 'spring'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103093107) do
+ActiveRecord::Schema.define(version: 20161101091917) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -18,9 +21,7 @@ ActiveRecord::Schema.define(version: 20161103093107) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,8 +30,6 @@ ActiveRecord::Schema.define(version: 20161103093107) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,16 +37,11 @@ ActiveRecord::Schema.define(version: 20161103093107) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.time     "activated_at"
-    t.string   "latitude"
-    t.string   "longitude"
-    t.string   "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["login"], name: "index_users_on_login", unique: true
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["login"], name: "index_users_on_login", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "posts"
 end
