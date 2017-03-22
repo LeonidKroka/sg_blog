26aa24bc47586f62df111ae010418cb0820ba4b6
kroka_l <sg.leonidK@gmail.com>
2017-03-22 13:52:23 +0200

    Change precompile config.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module SgBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.scss *.js)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.scss *.js)
   end
 end
